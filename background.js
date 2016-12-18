e88dcda2e665b7cda64c895914dde8ce4bea6b50
jaytchheda <jaytchheda@gmail.com>
2016-12-17 22:24:02 -0600

    Add files via upload
    Added context menu support

--- background.js
+++ background.js
@@ -4,8 +4,34 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
     chrome.browserAction.setBadgeText({text: message});
 });
 
+chrome.tabs.onHighlighted.addListener(function(){
+	
+	chrome.tabs.executeScript(null, {file: "content-script.js"});
+	
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	if(changeInfo.status == "complete") {
 		chrome.tabs.executeScript(null, {file: "content-script.js"});
 	}
+});
+
+addToStorage = function(word){
+    var query = word.selectionText;
+    chrome.storage.local.get('keywords', function (result) {
+		
+		var keywordsArray = [];
+		if(result.keywords)
+			keywordsArray = result.keywords;
+		keywordsArray.push(query);
+		chrome.storage.local.set({keywords: keywordsArray}, function(){
+			console.log('saved');
+		});
+	});
+ };
+
+chrome.contextMenus.create({
+ title: "Add to K4 Job Analyzer",
+ contexts:["selection"],  // ContextType
+ onclick: addToStorage // A callback function
 });
--- content-script.js
+++ content-script.js
@@ -1,11 +1,11 @@
-	console.log('caught');
-	var elements = document.getElementsByTagName('*');
-	var count = 0
-	var keywordsArray;
-	chrome.storage.local.get('keywords', function (result) {
-		keywordsArray = result.keywords;
-		console.log(result);
-		for (var i = 0; i < elements.length; i++) {
+console.log('caught');
+var elements = document.getElementsByTagName('*');
+var count = 0
+var keywordsArray;
+chrome.storage.local.get('keywords', function (result) {
+	keywordsArray = result.keywords;
+	//console.log(result);
+	/*	for (var i = 0; i < elements.length; i++) {
 		var element = elements[i];
 
 		for (var j = 0; j < element.childNodes.length; j++) {
@@ -22,11 +22,32 @@
 				}
 			}
 		}
+	}*/
+
+	var text = document.body.innerHTML;
+	for(var index in keywordsArray)
+	{
+		var keyword = keywordsArray[index];
+		if(text)
+		{
+			
+			var res = text.match(new RegExp(escapeRegExp(keyword), "i"));
+			console.log(escapeRegExp(keyword));
+			if(res)
+				count += res.length;
+		}
 	}
+
 	console.log(count+" found");
 
 	chrome.runtime.sendMessage(count.toString());
-	});
+});
+
+	
+function escapeRegExp(str) {
+	str = str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  return str.replace(/\s+/g, "\\s+");
+}
 	
 	
 
